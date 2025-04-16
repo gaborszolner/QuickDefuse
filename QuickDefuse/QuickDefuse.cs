704406254b9b1dba40e923e187dd6336d22505a7
Sinistral <gaborszolner@yahoo.com>
2025-04-16 15:39:26 +0200

    Bugfix: cant chose wire more times anymore Add chatcolors to messages Add !thetime command Code cleanup

--- QuickDefuse/QuickDefuse.cs
+++ QuickDefuse/QuickDefuse.cs
@@ -4,6 +4,8 @@ using CounterStrikeSharp.API.Modules.Menu;
 using Microsoft.Extensions.Logging;
 using CounterStrikeSharp.API.Modules.Commands;
 using CounterStrikeSharp.API.Modules.Events;
+using CounterStrikeSharp.API.Modules.Utils;
+using System.Drawing;
 
 namespace QuickDefuse
 {
@@ -19,7 +21,8 @@ namespace QuickDefuse
         private static Wire _triedWire = Wire.NotDefined;
         private static CPlantedC4? plantedBomb;
         private static CCSPlayerController? planterPlayer = null;
-
+        private static bool playerPlantAlreadyChosen = false;
+        private static bool playerDefuseAlreadyChosen = false;
         enum Wire
         {
             NotDefined = 0,
@@ -63,6 +66,8 @@ namespace QuickDefuse
             _rightWire = (Wire)new Random().Next(1, 5);
             plantedBomb = null;
             planterPlayer = null;
+            playerDefuseAlreadyChosen = false;
+            playerPlantAlreadyChosen = false;
             return HookResult.Continue;
         }
 
@@ -76,13 +81,18 @@ namespace QuickDefuse
             return HookResult.Continue;
         }
 
-        public HookResult OnPlayerChat(EventPlayerChat @event, GameEventInfo info)
+        public static HookResult OnPlayerChat(EventPlayerChat @event, GameEventInfo info)
         {
             var player = Utilities.GetPlayerFromUserid(@event.Userid);
 
             if (@event?.Text.Trim().ToLower() == "!quickdefuse")
             {
-                player?.PrintToChat("To defuse quickly, bind a key. For example, to cut wire 3 with the K key, type: bind k \"3\"");
+                player?.PrintToChat("To defuse quickly, bind a key. \nFor example, to cut wire 3 with the K key, type: bind k \"3\"");
+                return HookResult.Handled;
+            }
+            else if (@event?.Text.Trim().ToLower() == "!thetime")
+            {
+                player?.PrintToChat($"{DateTime.Now}");
                 return HookResult.Handled;
             }
 
@@ -109,12 +119,31 @@ namespace QuickDefuse
                     Task.Delay(1000).Wait();
                 }
                 MenuManager.CloseActiveMenu(player);
-                if (isPlant) 
+                if (isPlant)
                 {
                     player.PrintToChat($"You chose {_rightWire}!");
                 }
 
             });
+        }
+
+        private static char GetChatColor(Wire rightWire)
+        {
+            switch (rightWire)
+            {
+                case Wire.Green:
+                    return ChatColors.Green;
+                case Wire.Yellow:
+                    return ChatColors.Yellow;
+                case Wire.Red:
+                    return ChatColors.Red;
+                case Wire.Blue:
+                    return ChatColors.Blue;
+                case Wire.NotDefined:
+                case Wire.Random:
+                default:
+                    return ChatColors.Default;
+            }
         }
 
         #region Plant
@@ -134,25 +163,17 @@ namespace QuickDefuse
 
         private HookResult OnKeyPressPlant(CCSPlayerController? player, CommandInfo command)
         {
-            if (player is not null && planterPlayer is not null && player == planterPlayer)
+            if (player is not null && planterPlayer is not null && player == planterPlayer && !playerPlantAlreadyChosen)
             {
-                int selectedOption = command.GetCommandString switch
+                playerPlantAlreadyChosen = true;
+                switch (command.GetCommandString)
                 {
-                    "1" => 1,
-                    "2" => 2,
-                    "3" => 3,
-                    "4" => 4,
-                    "5" => 5,
-                    _ => 5
-                };
-
-                switch (selectedOption)
-                {
-                    case 1: GreenPlantAction(player, null); break;
-                    case 2: YellowPlantAction(player, null); break;
-                    case 3: RedPlantAction(player, null); break;
-                    case 4: BluePlantAction(player, null); break;
-                    case 5: RandomPlantAction(player, null); break;
+                    case "1": GreenPlantAction(player, null); break;
+                    case "2": YellowPlantAction(player, null); break;
+                    case "3": RedPlantAction(player, null); break;
+                    case "4": BluePlantAction(player, null); break;
+                    case "5": RandomPlantAction(player, null); break;
+                    default: RandomPlantAction(player, null); break;
                 }
             }
             return HookResult.Continue;
@@ -160,7 +181,7 @@ namespace QuickDefuse
 
         private HookResult OnBombPlantedCommand(EventBombPlanted @event, GameEventInfo info)
         {
-            Server.PrintToChatAll($"The bomb can be defused by cutting the correct wire. For help type !quickdefuse.");
+            Server.PrintToChatAll($"The bomb can be defused by cutting the correct wire.\nFor help type !quickdefuse.");
             return HookResult.Continue;
         }
 
@@ -168,36 +189,42 @@ namespace QuickDefuse
         {
             _rightWire = Wire.Green;
             MenuManager.CloseActiveMenu(player);
-            player.PrintToChat($"You chose {_rightWire}!");
+            PrintYouChose(player, _rightWire);
         }
 
         private static void YellowPlantAction(CCSPlayerController player, ChatMenuOption option)
         {
             _rightWire = Wire.Yellow;
             MenuManager.CloseActiveMenu(player);
-            player.PrintToChat($"You chose {_rightWire}!");
+            PrintYouChose(player, _rightWire);
         }
 
         private static void RedPlantAction(CCSPlayerController player, ChatMenuOption option)
         {
             _rightWire = Wire.Red;
             MenuManager.CloseActiveMenu(player);
-            player.PrintToChat($"You chose {_rightWire}!");
+            PrintYouChose(player, _rightWire);
         }
 
         private static void BluePlantAction(CCSPlayerController player, ChatMenuOption option)
         {
             _rightWire = Wire.Blue;
             MenuManager.CloseActiveMenu(player);
-            player.PrintToChat($"You chose {_rightWire}!");
+            PrintYouChose(player, _rightWire);
         }
 
         private static void RandomPlantAction(CCSPlayerController player, ChatMenuOption option)
         {
-            Random rand = new();
-            _rightWire = (Wire)rand.Next(1, 5);
+            _rightWire = (Wire)new Random().Next(1, 5);
             MenuManager.CloseActiveMenu(player);
-            player.PrintToChat($"You chose {_rightWire}!");
+            PrintYouChose(player, _rightWire);
+        }
+
+        private static void PrintYouChose(CCSPlayerController player, Wire rightWire)
+        {
+            char color = GetChatColor(rightWire);
+
+            player.PrintToChat($"You chose {color}{_rightWire}{ChatColors.Default}!");
         }
 
         private HookResult OnBombAbortPlant(EventBombAbortplant @event, GameEventInfo info)
@@ -207,6 +234,7 @@ namespace QuickDefuse
                 return HookResult.Continue;
 
             planterPlayer = null;
+            playerPlantAlreadyChosen = false;
             _rightWire = Wire.NotDefined;
             MenuManager.GetActiveMenu(player)?.Close();
 
@@ -223,6 +251,7 @@ namespace QuickDefuse
                 return HookResult.Continue;
 
             _triedWire = Wire.NotDefined;
+            playerDefuseAlreadyChosen = false;
             MenuManager.GetActiveMenu(player)?.Close();
 
             return HookResult.Continue;
@@ -238,7 +267,7 @@ namespace QuickDefuse
             var player = @event.Userid;
 
             plantedBomb = FindPlantedBomb();
-            if (plantedBomb is null) 
+            if (plantedBomb is null)
             {
                 return HookResult.Continue;
             }
@@ -250,30 +279,17 @@ namespace QuickDefuse
 
         private HookResult OnKeyPressDefuse(CCSPlayerController? player, CommandInfo command)
         {
-            if (player is not null && plantedBomb is not null && plantedBomb.BeingDefused && plantedBomb.BombDefuser == player.Pawn)
+            if (player is not null && plantedBomb is not null && plantedBomb.BeingDefused && plantedBomb.BombDefuser == player.Pawn && !playerDefuseAlreadyChosen)
             {
-                int selectedOption = command.GetCommandString switch
-                {
-                    "1" => 1,
-                    "2" => 2,
-                    "3" => 3,
-                    "4" => 4,
-                    "5" => 5,
-                    _ => 5
-                };
+                playerDefuseAlreadyChosen = true;
 
-                switch (selectedOption)
+                switch (command.GetCommandString)
                 {
-                    case 1: CutBombWire(Wire.Green); break;
-                    case 2: CutBombWire(Wire.Yellow); break;
-                    case 3: CutBombWire(Wire.Red); break;
-                    case 4: CutBombWire(Wire.Blue); break;
-                    case 5:
-                        {
-                            Random rand = new();
-                            CutBombWire((Wire)rand.Next(1, 5));
-                            break;
-                        }
+                    case "1": CutBombWire(Wire.Green); break;
+                    case "2": CutBombWire(Wire.Yellow); break;
+                    case "3": CutBombWire(Wire.Red); break;
+                    case "4": CutBombWire(Wire.Blue); break;
+                    case "5": CutBombWire((Wire)new Random().Next(1, 5));  break;
                 }
             }
 
@@ -302,8 +318,7 @@ namespace QuickDefuse
 
         private static void RandomDefuseAction(CCSPlayerController player, ChatMenuOption option)
         {
-            Random rand = new();
-            CutBombWire((Wire)rand.Next(1, 5));
+            CutBombWire((Wire)new Random().Next(1, 5));
         }
 
         private static void CutBombWire(Wire triedWire)
@@ -318,14 +333,14 @@ namespace QuickDefuse
             {
                 Server.NextFrame(() =>
                 {
-                    Server.PrintToChatAll($"Bomb has been defused by cutting the right {_rightWire} wire!");
+                    Server.PrintToChatAll($"Bomb has been defused by cutting the right {GetChatColor(_rightWire)}{_rightWire}{ChatColors.Default} wire!");
                     plantedBomb.DefuseCountDown = 0;
                     plantedBomb.BombDefused = true;
                 });
             }
             else
             {
-                Server.PrintToChatAll($"Tried wire was {_triedWire}, but the right wire was {_rightWire}");
+                Server.PrintToChatAll($"Tried wire was {GetChatColor(_triedWire)}{_triedWire}{ChatColors.Default}, but the right wire was {GetChatColor(_rightWire)}{_rightWire}{ChatColors.Default}");
                 plantedBomb.CannotBeDefused = true;
                 plantedBomb.C4Blow = 1;
             }
